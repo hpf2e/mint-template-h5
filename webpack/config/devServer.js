f85b97795459e3427f92a80028d3e1ce04c888ec
陈晓刚 <chenxiaogang@hupu.com>
2021-01-05 15:04:15 +0800

    fix: 根据ip打卡

--- webpack/config/devServer.js
+++ webpack/config/devServer.js
@@ -1,13 +1,13 @@
 /**
  * @see https://webpack.js.org/configuration/dev-server/
  */
-import isWindows from 'is-windows';
+import ip from 'ip';
 
 import {devServerProxyConfig} from './devServierProxy';
 
 const defaultPort = 8080;
 
-const devServerHost = isWindows() ? '127.0.0.1' : '0.0.0.0';
+const devServerHost = ip.address() ?? '127.0.0.1';
 
 export const devServerUrl = `http://${devServerHost}:${defaultPort}/`;
 
